e0d1fb5529f181105045198fdd0f0ba45037b3cc
watermouth <sminakuchi@gmail.com>
2014-09-09 07:19:51 +0900

    Eigen settings

---
+++ include/kalman_filter_core.h
@@ -0,0 +1,14 @@
+#ifndef include/kalman_filter_core_h
+#define include/kalman_filter_core_h
+
+#include "Eigen/Dense"
+
+namespace KalmanFilter {
+
+struct KalmanFilterCore {
+  
+};
+
+}
+
+#endif
---
+++ test/eigen_test.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "Eigen/Dense"
+
+TEST(EigenTest, FirstExample){
+  using Eigen::MatrixXd;
+  MatrixXd m(2,2);
+  m(0,0) = 3;
+  m(1,0) = 2.5;
+  m(0,1) = -1;
+  m(1,1) = m(1,0) + m(0,1);
+  std::cout << m << std::endl;
+}
