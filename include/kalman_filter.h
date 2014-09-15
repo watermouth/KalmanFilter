eee2ebfe0fa6e224b6d134f74ef3c0ec46508173
watermouth <sminakuchi@gmail.com>
2014-09-15 12:44:47 +0900

    removed kalman_filter.cpp

--- include/kalman_filter.h
+++ include/kalman_filter.h
@@ -29,12 +29,25 @@ struct KalmanFilter {
   KalmanFilter(const Eigen::MatrixXd &F,
                const Eigen::MatrixXd &V,
                const Eigen::MatrixXd &G,
-               const Eigen::MatrixXd &W);
+               const Eigen::MatrixXd &W)
+    : F_(F), V_(V), G_(G), W_(W)
+    , core_(new KalmanFilterCore)
+  {
+    /// モデルの設定
+    core_->F_ = &F_;
+    core_->V_ = &V_;
+    core_->G_ = &G_;
+    core_->W_ = &W_;
+  }
   /// 初期分布の設定
-  void SetInitialDistribution(const Eigen::VectorXd &m0, const Eigen::MatrixXd &C0);
+  void SetInitialDistribution(const Eigen::VectorXd &m0, const Eigen::MatrixXd &C0) {
+    core_->SetInitialDistribution(m0, C0);
+  }
   /// 予測・フィルタリングステップ
   /// \param[in] y 観測値
-  void Filtering(const Eigen::VectorXd &y);
+  void Filtering(const Eigen::VectorXd &y) {
+    core_->UpdateDistributions(y);
+  }
 };
 
 }
